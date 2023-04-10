bba3a316d07e26ffa25866c7f9e6e0aac04f2b17
Asia <asiaverner666@gmail.com>
2023-04-10 15:05:59 +0300

    Задание 3

---
+++ Задание_3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Создаем массив из 8 элементов
+        int[] numbers = {1, 2, 5, 7, 19, 6, 1, 33};
+
+        // Выводим элементы массива на экран
+        Console.WriteLine("[{0}]", string.Join(", ", numbers));
+    }
+}
