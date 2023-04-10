e33046750594b158d8367bcf9a573dcf540d57b5
Asia <asiaverner666@gmail.com>
2023-04-10 15:01:03 +0300

    Задание 1

---
+++ Задание_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите число A:");
+        int a = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Введите степень B:");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+
+        Console.WriteLine("{0} в степени {1} равно {2}", a, b, result);
+    }
+}
+
+
+
