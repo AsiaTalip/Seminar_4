2e9a10b4cfbd1d3ecdc4c5c1a5ee896ca8c0e55d
Asia <asiaverner666@gmail.com>
2023-04-10 15:03:33 +0300

    Задание 2

---
+++ Задание_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        Console.WriteLine("Сумма цифр в числе: " + sum);
+    }
+}
